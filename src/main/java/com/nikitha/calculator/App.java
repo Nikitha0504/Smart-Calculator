04f19e071d34a62b824282d3181ab2d09d6ba707
Nikitha0504 <129878089+Nikitha0504@users.noreply.github.com>
2025-11-14 13:52:45 +0530

    Add files via upload

---
+++ src/main/java/com/nikitha/calculator/App.java
@@ -0,0 +1,10 @@
+package com.nikitha.calculator;
+
+import com.nikitha.calculator.ui.CalculatorUI;
+
+public class App {
+
+    public static void main(String[] args) {
+        new CalculatorUI();
+    }
+}
